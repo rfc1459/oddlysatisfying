a8b71f1eba2b9eb64f2227bf39348a792121a08a
Matteo Panella <morpheus@level28.org>
2021-12-28 21:57:12 +0100

    Turn vtp into a slice

--- main.go
+++ main.go
@@ -1,74 +1,66 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "math/rand"
-    "os"
-    "strconv"
-    "time"
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 type uselessFunc func()
 
-var vtp = map[int]uselessFunc{
-    0:  download,
-    1:  mvs,
-    2:  bootVax,
-    3:  c64,
-    4:  adminports,
-    5:  fsck,
-    6:  bootMsDos,
-    7:  stateLinux,
-    8:  qradarAutoupdate,
-    9:  pfSenseMenu,
-    10: vm370,
-    11: ciscoboot,
-    12: curlDownload,
+var vtp = []uselessFunc{
+	download,
+	mvs,
+	bootVax,
+	c64,
+	adminports,
+	fsck,
+	bootMsDos,
+	stateLinux,
+	qradarAutoupdate,
+	pfSenseMenu,
+	vm370,
+	ciscoboot,
+	curlDownload,
 }
 
-var clo = map[int]uselessFunc{
-    0: standard,
+var clo = []uselessFunc{
+	standard,
 }
 
 func main() {
-    args := os.Args[1:]
-    rand.Seed(time.Now().UnixNano())
-    calls := rand.Intn(len(vtp))
-    var err error
-    if len(args) > 0 {
-        calls = 0
-        calls, err = strconv.Atoi(args[0])
-        if err != nil {
-            fmt.Print("ok, you tried to pass the number of how many program execute...\n")
-            fmt.Printf("but what you type is not a number [%v]\n", args[0])
-            fmt.Printf("so you will have the default for now :)\n")
-        }
-    }
-    calls = int(math.Min(float64(calls), float64(len(vtp))))
-    if calls == 0 {
-        calls = 1
-    }
-    fmt.Printf("here we go!  [%v]\n\n\n\n\n", calls)
+	args := os.Args[1:]
+	rand.Seed(time.Now().UnixNano())
+	calls := rand.Intn(len(vtp))
 
-    // create execution list
-    myList := []int{}
+	if len(args) > 0 {
+		var err error
+		calls = 0
+		calls, err = strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Print("ok, you tried to pass the number of how many program execute...\n")
+			fmt.Printf("but what you type is not a number [%v]\n", args[0])
+			fmt.Printf("so you will have the default for now :)\n")
+		}
+	}
+	calls = int(math.Min(float64(calls), float64(len(vtp))))
+	if calls == 0 {
+		calls = 1
+	}
 
-    for i := 0; i < len(vtp); i++ {
-        myList = append(myList, i)
-    }
+	fmt.Printf("here we go!  [%v]\n\n\n\n\n", calls)
 
-    rand.Seed(time.Now().UnixNano())
-    rand.Shuffle(len(myList), func(i, j int) { myList[i], myList[j] = myList[j], myList[i] })
+	rand.Shuffle(len(vtp), func(i, j int) { vtp[i], vtp[j] = vtp[j], vtp[i] })
 
-    myList = myList[:calls]
+	vtp = vtp[:calls]
 
-    for i := 0; i < len(myList); i++ {
-    	call := vtp[myList[i]]
-    	call()
-    }
+	for _, call := range vtp {
+		call()
+	}
 
-
-    call := clo[rand.Intn(len(clo))]
-    call()
+	call := clo[rand.Intn(len(clo))]
+	call()
 }
